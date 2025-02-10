5b27e44e90fc629fee61121d5369d5149ca57acf
Kangbit <kangbit@smartscore.kr>
2025-02-10 16:11:33 +0900

    입력 텍스트필드 변경

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Chat(
-          messages: _messages,
-          onSendPressed: _handleSendPressed,
-          user: _user,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const ChatEditBar(),
+            SizedBox(height: MediaQuery.of(context).padding.bottom)
+          ],
         ),
       );
+
+  // @override
+  // Widget build(BuildContext context) => Scaffold(
+  //       body: Chat(
+  //         messages: _messages,
+  //         onSendPressed: _handleSendPressed,
+  //         user: _user,
+  //       ),
+  //     );
 
   void _addMessage(types.Message message) {
     setState(() {
@@ -60,5 +71,97 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     _addMessage(textMessage);
+  }
+}
+
+class ChatEditBar extends StatefulWidget {
+  const ChatEditBar({super.key});
+
+  @override
+  State<ChatEditBar> createState() => _ChatEditBarState();
+}
+
+class _ChatEditBarState extends State<ChatEditBar> {
+  final TextEditingController _editController =
+      TextEditingController(text: 'Message?');
+  final FocusNode _focusNode = FocusNode();
+
+  void _send(String query) {
+    if (query.isEmpty) return;
+
+    // context.read(chatListProvider.notifier).add(
+    //       Chat(
+    //         request: ChatRequest(
+    //           model: dotenv.env['CHAT_MODEL']!,
+    //           messages: [Message(content: query)],
+    //         ),
+    //       ),
+    //     );
+
+    _editController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    _editController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      height: 48,
+      child: Row(
+        children: [
+          Expanded(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(28.0),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 2),
+                    blurRadius: 4,
+                    color: Colors.grey,
+                  )
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
+                        hintText: "Message?",
+                        border: InputBorder.none,
+                      ),
+                      textInputAction: TextInputAction.send,
+                      focusNode: _focusNode,
+                      controller: _editController,
+                      onSubmitted: _send,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.graphic_eq),
+                  ),
+                  const SizedBox(width: 8.0),
+                ],
+              ),
+            ),
+          ),
+          IconButton(
+            padding: EdgeInsets.zero,
+            alignment: Alignment.centerRight,
+            iconSize: 32.0,
+            onPressed: () => _send(_editController.text),
+            icon: const Icon(Icons.arrow_circle_up_rounded),
+          ),
+        ],
+      ),
+    );
   }
 }
